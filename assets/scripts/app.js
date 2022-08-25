f0db3ea0a97e3164d65fa31cdf7a9b1a713a06d0
Lucas Debarbieri <lucas1702debarbieri@gmail.com>
2022-08-25 12:28:45 -0300

    hola

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,3 +2,5 @@ window.addEventListener("scroll", function () {
     var navbar = this.document.querySelector("nav");
     navbar.classList.toggle("sticky", window.scrollY > 50);
 });
+
+const imgArr = [];
