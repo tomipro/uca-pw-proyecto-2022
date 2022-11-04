c6ef8c3944f527e3864c5e82f6e50b7d4b8d0980
Lucas Debarbieri <lucas1702debarbieri@gmail.com>
2022-11-03 23:20:24 -0300

    Base de datos

--- assets/sql/uca-pw-proyecto-DB (1).sql
+++ assets/sql/uca-pw-proyecto-DB (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 13, 2022 at 06:56 AM
+-- Generation Time: Nov 04, 2022 at 03:19 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.19
 
@@ -30,10 +30,10 @@ SET time_zone = "+00:00";
 CREATE TABLE `Arte` (
   `Id_Arte` int(11) NOT NULL,
   `Id_Coleccion` int(11) NOT NULL,
-  `Precio` float NOT NULL,
   `User_Id` int(11) NOT NULL,
   `Privado` tinyint(1) NOT NULL DEFAULT 0,
-  `Arte_Img` blob NOT NULL
+  `Arte_Img` blob NOT NULL,
+  `Precio` decimal(11,11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -45,6 +45,21 @@ CREATE TABLE `Arte` (
 CREATE TABLE `Colecciones` (
   `Id_Coleccion` int(11) NOT NULL,
   `Nombre_Coleccion` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Compras`
+--
+
+CREATE TABLE `Compras` (
+  `Comprador` int(11) NOT NULL,
+  `Vendedor` int(11) NOT NULL,
+  `Id_compra` int(11) NOT NULL,
+  `Id_arte` int(11) NOT NULL,
+  `Precio` decimal(11,11) NOT NULL,
+  `Hora` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -62,6 +77,13 @@ CREATE TABLE `Usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `Usuarios`
+--
+
+INSERT INTO `Usuarios` (`User_Id`, `User_Email`, `User_Password`, `User_Level`, `User_Alias`) VALUES
+(1, 'lucasdeb@uca.edu.ar', 'Nashe', 1, 'vitalik_buterin');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -70,6 +92,7 @@ CREATE TABLE `Usuarios` (
 --
 ALTER TABLE `Arte`
   ADD PRIMARY KEY (`Id_Arte`,`Id_Coleccion`,`User_Id`),
+  ADD UNIQUE KEY `PRECIO` (`Precio`),
   ADD UNIQUE KEY `UNIQUE` (`Arte_Img`) USING HASH,
   ADD KEY `User_Id` (`User_Id`),
   ADD KEY `arte_ibfk_2` (`Id_Coleccion`);
@@ -80,6 +103,16 @@ ALTER TABLE `Arte`
 ALTER TABLE `Colecciones`
   ADD PRIMARY KEY (`Id_Coleccion`),
   ADD UNIQUE KEY `UNIQUE` (`Nombre_Coleccion`);
+
+--
+-- Indexes for table `Compras`
+--
+ALTER TABLE `Compras`
+  ADD PRIMARY KEY (`Id_compra`),
+  ADD UNIQUE KEY `UNIQUE` (`Comprador`),
+  ADD UNIQUE KEY `UNIQUE 2` (`Vendedor`),
+  ADD UNIQUE KEY `Arte` (`Id_arte`),
+  ADD UNIQUE KEY `PRECIO` (`Precio`);
 
 --
 -- Indexes for table `Usuarios`
@@ -108,7 +141,7 @@ ALTER TABLE `Colecciones`
 -- AUTO_INCREMENT for table `Usuarios`
 --
 ALTER TABLE `Usuarios`
-  MODIFY `User_Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `User_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -119,7 +152,16 @@ ALTER TABLE `Usuarios`
 --
 ALTER TABLE `Arte`
   ADD CONSTRAINT `arte_ibfk_1` FOREIGN KEY (`User_Id`) REFERENCES `Usuarios` (`User_Id`),
-  ADD CONSTRAINT `arte_ibfk_2` FOREIGN KEY (`Id_Coleccion`) REFERENCES `Colecciones` (`Id_Coleccion`);
+  ADD CONSTRAINT `arte_ibfk_2` FOREIGN KEY (`Id_Coleccion`) REFERENCES `Colecciones` (`Id_Coleccion`),
+  ADD CONSTRAINT `arte_ibfk_3` FOREIGN KEY (`Id_Arte`) REFERENCES `Compras` (`Id_arte`),
+  ADD CONSTRAINT `arte_ibfk_4` FOREIGN KEY (`Precio`) REFERENCES `Compras` (`Precio`);
+
+--
+-- Constraints for table `Compras`
+--
+ALTER TABLE `Compras`
+  ADD CONSTRAINT `compras_ibfk_1` FOREIGN KEY (`Comprador`) REFERENCES `Usuarios` (`User_Id`),
+  ADD CONSTRAINT `compras_ibfk_2` FOREIGN KEY (`Vendedor`) REFERENCES `Usuarios` (`User_Id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
