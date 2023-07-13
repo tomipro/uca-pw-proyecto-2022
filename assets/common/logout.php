8ee4866219692a01fa4c5b6c33553a4c84a8d4eb
Lucas Debarbieri <lucas1702debarbieri@gmail.com>
2023-07-13 05:27:07 -0300

    It actually works

--- assets/common/logout.php
+++ assets/common/logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy();
-header("Location: http://localhost/pwebcrm/login.php");
+header("Location: http://localhost/uca-pw-proyecto-2022/assets/common/login.php");
 die();
 ?>
